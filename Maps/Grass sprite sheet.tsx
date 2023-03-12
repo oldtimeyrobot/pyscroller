8cee06e1c82624ba1873e02fc5247e97422c6013
briostheman <briostheman@gmail.com>
2023-03-12 18:26:16 -0400

    Fixed world offset scroll with player issue, need to build a better level

---
+++ Maps/Grass sprite sheet.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Grass sprite sheet" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../Sprites/Grass sprite sheet.png" width="112" height="112"/>
+ <tile id="0">
+  <properties>
+   <property name="ground" type="int" value="1"/>
+   <property name="solid" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="ground" type="int" value="0"/>
+   <property name="solid" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="ground" type="int" value="0"/>
+   <property name="solid" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="ground" type="int" value="0"/>
+   <property name="solid" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="ground" type="int" value="0"/>
+   <property name="solid" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="ground" type="int" value="0"/>
+   <property name="solid" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="ground" type="int" value="0"/>
+   <property name="solid" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="ground" type="int" value="0"/>
+   <property name="solid" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="ground" type="int" value="0"/>
+   <property name="solid" type="int" value="0"/>
+  </properties>
+ </tile>
+</tileset>
